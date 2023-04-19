6772400c5c9299a32edee258a8bdd6ebc08edd7b
iago-monteirog <iagomgarcia9000@gmail.com>
2023-04-18 21:54:16 -0300

    WIP: carrosel cards serviços

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions, SlidesOutputData } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  activeSlides: SlidesOutputData = new SlidesOutputData;
+
+  constructor() {}
+
+  getData(data: SlidesOutputData) {
+      this.activeSlides = data;
+        console.log(this.activeSlides);
+  }
+
+  carouselOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 600,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    responsive: {
+        0: {
+            items: 4,
+        },
+    }, 
+    nav: false,
+};
+
   currentIndex = 0;
   itemsCardService = [
-    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.' },
-    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.' },
-    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.' },
-    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '1' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '2' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '3' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '4' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '5' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '6' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '7' },
+    { imageService: '../../../assets/images/iconExemple.png', alt: '...', titleService: 'Nomenclatura do Serviço', description: 'Breve descritivo sobre o serviço ou exemplos de aplicações.', id: '8' },
+
   ];
   itemsCaseSucess = [
     { imageCase: '../../../assets/images/nubankCase.png', alt: '...', titleCase: 'Case de Sucesso com Nubank', imagePartner1: '../../../assets/images/nu_Hover.png', imagePartner2: '../../../assets/images/solvo_white.png', partnerCase1: 'Nubank', partnerCase2: 'Solvo', case: '/cases/case'  },
