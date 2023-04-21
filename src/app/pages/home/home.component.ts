1f9a14fe154b5fadb7e49cb7aabab49721850e45
iago-monteirog <iagomgarcia9000@gmail.com>
2023-04-21 19:40:21 -0300

    WIP: carrossel mobile cases homem

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OwlOptions, SlidesOutputData } from 'ngx-owl-carousel-o';
 
 @Component({
@@ -6,15 +6,50 @@ import { OwlOptions, SlidesOutputData } from 'ngx-owl-carousel-o';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   activeSlides: SlidesOutputData = new SlidesOutputData;
 
-  constructor() {}
+  isMobile: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isMobile = window.screen.width < 768;
+
+    const carousel = document.querySelector('.owl-carousel') as HTMLElement;
+
+    if (carousel) {
+      carousel.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+      }, { passive: false });
+    }
+  }
+
+  isMobileScreen(): boolean {
+    return this.isMobile;
+  }
 
   getData(data: SlidesOutputData) {
-      this.activeSlides = data;
-        console.log(this.activeSlides);
+    this.activeSlides = data;
+    console.log(this.activeSlides);
   }
+
+  carouselOptionsMobile: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 600,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    responsive: {
+      200: {
+        items: 1,
+      },
+    },
+    nav: false,
+  };
 
   carouselOptions: OwlOptions = {
     loop: true,
@@ -26,18 +61,18 @@ export class HomeComponent {
     autoplay: false,
     autoplayTimeout: 5000,
     responsive: {
-        400: {
-            items: 1,
-        },
-        600: {
-          items: 3
-        },
-        1200: {
-          items: 4
-        }
-    }, 
+      400: {
+        items: 1,
+      },
+      600: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      }
+    },
     nav: false,
-};
+  };
 
   currentIndex = 0;
   itemsCardService = [
@@ -52,10 +87,10 @@ export class HomeComponent {
 
   ];
   itemsCaseSucess = [
-    { imageCase: '../../../assets/images/nubankCase.png', alt: '...', titleCase: 'Case de Sucesso com Nubank', imagePartner1: '../../../assets/images/nu_Hover.png', imagePartner2: '../../../assets/images/solvo_white.png', partnerCase1: 'Nubank', partnerCase2: 'Solvo', case: '/cases/case'  },
-    { imageCase: '../../../assets/images/vivoCase.png', alt: '...', titleCase: 'Case de Sucesso com Vivo Brasil', imagePartner1: '../../../assets/images/vivo_Hover.png', imagePartner2: '../../../assets/images/wib_white.png', partnerCase1: 'Vivo Brasil', partnerCase2: 'Wib', case: '/cases/case'  },
-    { imageCase: '../../../assets/images/ifoodCase.png', alt: '...', titleCase: 'Case de Sucesso com Ifood', imagePartner1: '../../../assets/images/ifood_Hover.png', imagePartner2: '../../../assets/images/wib_white.png', partnerCase1: 'Ifood', partnerCase2: 'Wib', case: '/cases/case'  },
-    { imageCase: '../../../assets/images/aleloCase.png', alt: '...', titleCase: 'Case de Sucesso com Alelo', imagePartner1: '../../../assets/images/alelo_Hover.png', imagePartner2: '../../../assets/images/solvo_white.png', partnerCase1: 'Alelo', partnerCase2: 'Solvo', case: '/cases/case'  },
+    { imageCase: '../../../assets/images/nubankCase.png', alt: '...', titleCase: 'Case de Sucesso com Nubank', imagePartner1: '../../../assets/images/nu_Hover.png', imagePartner2: '../../../assets/images/solvo_white.png', partnerCase1: 'Nubank', partnerCase2: 'Solvo', case: '/cases/case' },
+    { imageCase: '../../../assets/images/vivoCase.png', alt: '...', titleCase: 'Case de Sucesso com Vivo Brasil', imagePartner1: '../../../assets/images/vivo_Hover.png', imagePartner2: '../../../assets/images/wib_white.png', partnerCase1: 'Vivo Brasil', partnerCase2: 'Wib', case: '/cases/case' },
+    { imageCase: '../../../assets/images/ifoodCase.png', alt: '...', titleCase: 'Case de Sucesso com Ifood', imagePartner1: '../../../assets/images/ifood_Hover.png', imagePartner2: '../../../assets/images/wib_white.png', partnerCase1: 'Ifood', partnerCase2: 'Wib', case: '/cases/case' },
+    { imageCase: '../../../assets/images/aleloCase.png', alt: '...', titleCase: 'Case de Sucesso com Alelo', imagePartner1: '../../../assets/images/alelo_Hover.png', imagePartner2: '../../../assets/images/solvo_white.png', partnerCase1: 'Alelo', partnerCase2: 'Solvo', case: '/cases/case' },
   ];
   current = 0;
 }
