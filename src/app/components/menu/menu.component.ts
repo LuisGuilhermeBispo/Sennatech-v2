93138ab357a0f6f193bdfadd92b613e063297b13
iago-monteirog <iagomgarcia9000@gmail.com>
2023-05-01 21:00:32 -0300

    WIP

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,23 +8,32 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
 
-    public sobre     : string = "/sobre"
-    public servicos  : string = "/servicos"
-    public cases     : string = "/cases"
-    public parceiros : string = "/parceiros"
-    public carreira  : string = "/carreira"
-    public contato  :  string = "/contato"
+  public sobre: string = "/sobre"
+  public servicos: string = "/servicos"
+  public cases: string = "/cases"
+  public parceiros: string = "/parceiros"
+  public carreira: string = "/carreira"
+  public contato: string = "/contato"
 
-    constructor(public router: Router) {
+  constructor(public router: Router) {
 
-    }
+  }
 
-  ngOnInit(): void {}
+  pagesList = [
+    { link: this.sobre, text: 'Sobre' },
+    { link: this.servicos, text: 'Serviços' },
+    { link: this.cases, text: 'Cases' },
+    { link: this.parceiros, text: 'Parceiros' },
+    { link: this.carreira, text: 'Carreira' },
+    { link: this.contato, text: 'Contato' },
+  ]
+
+  ngOnInit(): void { }
 
   getRoute() {
     if (this.router.url === '/home') {
       return "menu__home"
-    } else{
+    } else {
       return "menu__others"
     }
   }
